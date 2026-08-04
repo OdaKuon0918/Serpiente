6134522e36e1c66d71073c1c7c2b5af6ff99cdaf
ODAKUON0918 <tiisaitanbo0918@gmail.com>
2026-08-04 17:51:15 +0900

    update: LIFF関連修正

--- project/admin/js/eventList.js
+++ project/admin/js/eventList.js
@@ -1,6 +1,6 @@
 console.log("eventList.js 読み込み OK");
 
-import liff from 'https://static.line-scdn.net/liff/edge/2/sdk.js';
+import * as liff from 'https://static.line-scdn.net/liff/edge/2/sdk.js';
 
 async function initLiff() {
   await liff.init({ liffId: "2010961634-PvIMmuiT" });
